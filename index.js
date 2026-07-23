9880f2d2805854d0523e9e4d6344fa2436fe9d65
unsunnysideup <txsu.n000@gmail.com>
2026-07-22 22:27:12 -0400

    Added Form Functionality
    1. Added people to RSVP List
    2. Validates Form

--- index.js
+++ index.js
@@ -13,3 +13,68 @@ function buttonChange() {
    else themeButton.textContent = "Toggle Dark Mode";
 
 }
+
+let count = 3;
+document.getElementById("rsvp-form").addEventListener("submit", (event)=> {
+   event.preventDefault();
+   if (validateForm()) {
+   addParticipation(event);
+}});
+
+function addParticipation(event) {
+   let first_name = document.getElementById("first-name").value;
+   let last_name = document.getElementById("last-name").value;
+   let counter = document.getElementById("id-count");
+
+   if (counter) {
+      counter.remove();
+   }
+
+   const participant = document.createElement("p");
+   counter = document.createElement("p");
+   count = count + 1;
+   counter.id = "id-count";
+   participant.textContent = `✌︎㋡ ${first_name} ${last_name} has RSVPed!`;
+   counter.innerHTML= `${count} people has signed up! <br>
+            Come join the fun by filling out the form below ~ `
+   document.getElementById("rsvp-list").appendChild(participant);
+   document.getElementById("counter").appendChild(counter);
+
+
+   document.getElementById("rsvp-form").reset();
+}
+
+function validateForm() {
+
+   var isValid = true;
+   var rsvpInputs = document.getElementById("rsvp-form").elements;
+   
+   for (let i = 0; i < 3; i++) {
+      if (rsvpInputs[i].value.length < 2) {
+         rsvpInputs[i].classList.add('error');
+         isValid = false;
+      } else {
+         rsvpInputs[i].classList.remove('error');
+      };
+
+   };
+
+   let emailInput = rsvpInputs[2];
+   if (!emailInput.value.includes('@')) {
+      emailInput.classList.add('error');
+      isValid = false;
+   } else {
+      emailInput.classList.remove('error')
+   }
+
+   let phoneInput = rsvpInputs[3];
+   if (phoneInput.value.length != 10 ) {
+      phoneInput.classList.add('error');
+      isValid = false;
+   } else {
+      phoneInput.classList.remove('error')
+   }
+
+   return isValid
+
+}
