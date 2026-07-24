bcb3b4ca232ca4093464a8171da4c73008192687
unsunnysideup <txsu.n000@gmail.com>
2026-07-24 04:44:49 -0400

    Added id for modal image

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ document.getElementById("theme-button").addEventListener("click", () => {
 });
 
 function toggleDarkMode() {
-   document.body.classList.toggle("dark-mode"); 
+   document.body.classList.toggle("dark-mode");
 }
 
 function buttonChange() {
    let themeButton = document.getElementById("theme-button");
-   if (themeButton.textContent=="Toggle Dark Mode") themeButton.textContent = "Toggle Light Mode";
+   if (themeButton.textContent == "Toggle Dark Mode") themeButton.textContent = "Toggle Light Mode";
    else themeButton.textContent = "Toggle Dark Mode";
 
 }
@@ -29,7 +29,7 @@ function addParticipation(person) {
    count = count + 1;
    counter.id = "id-count";
    participant.textContent = `✌︎㋡ ${person.first_name} ${person.last_name} has RSVPed!`;
-   counter.innerHTML= `${count} people has signed up! <br>
+   counter.innerHTML = `${count} people has signed up! <br>
             Come join the fun by filling out the form below ~ `
    document.getElementById("rsvp-list").appendChild(participant);
    document.getElementById("counter").appendChild(counter);
@@ -47,7 +47,7 @@ function validateForm() {
       first_name: rsvpInputs[0].value,
       last_name: rsvpInputs[1].value
    };
-   
+
    for (let i = 0; i < 3; i++) {
       if (rsvpInputs[i].value.length < 2) {
          rsvpInputs[i].classList.add('error');
@@ -67,42 +67,45 @@ function validateForm() {
    }
 
    let phoneInput = rsvpInputs[3];
-   if (phoneInput.value.length != 10 ) {
+   if (phoneInput.value.length != 10) {
       phoneInput.classList.add('error');
       isValid = false;
    } else {
       phoneInput.classList.remove('error');
    }
 
-   if (isValid) addParticipation(person)
+   if (isValid) {
+      addParticipation(person);
+      toggleModal(person);
+   }
 
 }
 
-// Step 1: Select all elements with the class 'revealable'.
+// Selects all elements with the class 'revealable'.
 let revealableContainers = document.querySelectorAll('.revealable');
 
-// Step 2: Write function to reveal elements when they are in view.
+// Reveals elements when they are in view.
 const reveal = () => {
-    for (let i = 0; i < revealableContainers.length; i++) {
-        let current = revealableContainers[i];
+   for (let i = 0; i < revealableContainers.length; i++) {
+      let current = revealableContainers[i];
 
-        // Get current height of container and window
-        let windowHeight = window.innerHeight;
-        let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
-        let revealDistance = parseInt(getComputedStyle(current).getPropertyValue('--reveal-distance'), 10);
+      // Get current height of container and window
+      let windowHeight = window.innerHeight;
+      let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+      let revealDistance = parseInt(getComputedStyle(current).getPropertyValue('--reveal-distance'), 10);
 
-        // If the container is within range, add the 'active' class to reveal
-        if (topOfRevealableContainer < windowHeight - revealDistance) {
-            revealableContainers[i].classList.add('active');
-        }
-        // If the container is not within range, hide it by removing the 'active' class
-        else { 
-            revealableContainers[i].classList.remove('active');
-        }
-    }
+      // If the container is within range, add the 'active' class to reveal
+      if (topOfRevealableContainer < windowHeight - revealDistance) {
+         revealableContainers[i].classList.add('active');
+      }
+      // If the container is not within range, hide it by removing the 'active' class
+      else {
+         revealableContainers[i].classList.remove('active');
+      }
+   }
 }
 
-// Step 3: Whenever the user scrolls, check if any containers should be revealed
+// Whenever the user scrolls, check if any containers should be revealed
 window.addEventListener('scroll', reveal);
 
 document.getElementById('motion-button').addEventListener('click', reduceMotion);
@@ -110,33 +113,61 @@ document.getElementById('motion-button').addEventListener('click', reduceMotion)
 function reduceMotion() {
    let motionButton = document.getElementById("motion-button");
 
-   if (motionButton.textContent=="Reduce Motion Off") {
+   if (motionButton.textContent == "Reduce Motion Off") {
       for (let i = 0; i < revealableContainers.length; i++) {
-      revealableContainers[i].classList.add('reduceMotion');
+         revealableContainers[i].classList.add('reduceMotion');
       }
-
       motionButton.textContent = "Reduce Motion On";
+      return true;
+
    } else {
       for (let i = 0; i < revealableContainers.length; i++) {
          revealableContainers[i].classList.remove('reduceMotion')
       }
-
       motionButton.textContent = "Reduce Motion Off";
+      return false;
    }
 }
 
 // modal animations
 const toggleModal = (person) => {
-    let modal = 0; // TODO
-    
-    // TODO: Update modal display to flex
-    
+   let modal = document.getElementById("success-modal")
+   let modal_content = document.getElementById("modal-text")
 
-    // TODO: Update modal text to personalized message
+   // Update modal display to flex
+   modal.style.display = "flex";
 
+   // Update modal text to personalized message
+   modal_content.innerHTML = `Thanks for RSVPing, ${person.first_name}! <br><br>
+      You have been successfully registered for 2026 Pixel Fair. 
+      We can't wait to see you at the fair~`
 
-    // Set modal timeout to 5 seconds
-    
+   // Automatic Timeout
+   setTimeout(() => {
+      modal.style.display = "none";
+   }, 10000)
+
 }
 
-// TODO: animation variables and animateImage() function
+var rotateFactor = 5
+var modal_image = document.getElementById("modalImage")
+function animateImage() {
+   // rotation factor
+   rotateFactor = rotateFactor === 5? -5 : 5
+
+   // rotation animation
+   modal_image.style.transform = "rotate(" + rotateFactor + "deg)";
+   
+}
+
+let intervalId = setInterval(animateImage, 500);
+setTimeout(() => {
+   document.getElementById("success-modal").style.display = 'none';
+   clearInterval(intervalId);
+}, 5000);
+
+document.getElementById("leaveModal").addEventListener("click", exitModal);
+
+function exitModal() {
+   document.getElementById("success-modal").style.display="None";
+}
